ebad1708da965871a4d3d6c30ed232ff1fa46c9b
Anjali04040 <anjali04042018@gmail.com>
2024-06-27 15:59:07 +0530

    Project

---
+++ DAY15/config/db.js
@@ -0,0 +1,24 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://<username>:<password>@cluster0.lmxofcz.mongodb.net/<dbName>?appName=Cluster0";
+
+let dbURL = uri.replace('<username>', process.env.DB_USERNAME)
+    dbURL = dbURL.replace('<password>', process.env.DB_PASSWORD)
+    dbURL = dbURL.replace('<dbName>', process.env.DB_NAME)
+
+
+// const client = new MongoClient(dbURL);
+// const database = client.db(process.env.DB_NAME);
+// const productsCollection = database.collection("products");
+
+// module.exports = {
+//     productsCollection,
+// }
+
+mongoose.connect(dbURL).then(() =>{
+    console.log("--------- DB Connected -----------");
+})
+.catch((err) =>{
+    console.log("DB connect Failed: \n");
+    console.log(err);
+});
---
+++ DAY15/models/productModel.js
@@ -0,0 +1,32 @@
+const mongoose =  require("mongoose"); 
+
+const productSchema = new mongoose.Schema({
+    description : String,
+    title:{
+        type: String,
+        required : true,
+    },
+    price :{
+        type: Number,
+        required : true,
+    },
+    thumbnail :{
+        type: String,
+    },
+    images: {
+        description: String,
+        metadata :{},
+        createdAt :{
+            type: Date,
+            default: Date.now() ,
+        },
+        updatedAt:{
+            type: Date,
+            default: Date.now(),
+        }
+    }
+})
+
+const productModel = mongoose.model("product", productSchema);
+
+module.exports = productModel
